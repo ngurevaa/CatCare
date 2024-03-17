be50dcf95afead60de5597445d892fa65b37cd67
ngurevaa <nngurevaa@gmail.com>
2024-03-17 16:19:52 +0300

    initial commit

---
+++ app/src/main/java/ru/kpfu/itis/gureva/catcare/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.kpfu.itis.gureva.catcare.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.kpfu.itis.gureva.catcare.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
