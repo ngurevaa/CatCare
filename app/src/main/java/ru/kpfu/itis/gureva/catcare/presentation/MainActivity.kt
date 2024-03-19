26868ec001ef703d14b81ba3f21929801772acfb
ngurevaa <nngurevaa@gmail.com>
2024-03-19 13:29:40 +0300

    added Bottom Navigation View

--- app/src/main/java/ru/kpfu/itis/gureva/catcare/presentation/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/gureva/catcare/presentation/MainActivity.kt
@@ -3,10 +3,20 @@ package ru.kpfu.itis.gureva.catcare.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.kpfu.itis.gureva.catcare.R
+import ru.kpfu.itis.gureva.catcare.databinding.ActivityMainBinding
+import ru.kpfu.itis.gureva.catcare.presentation.repository.WelcomeSpeechRepository
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.main_container, HomeFragment())
+                .commit()
+        }
     }
 }
