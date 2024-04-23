5c8fe26ba941d46a19dbd795471d6d6098da5744
ngurevaa <nngurevaa@gmail.com>
2024-04-23 18:29:56 +0300

    added profile design

--- app/src/main/java/ru/kpfu/itis/gureva/catcare/presentation/screens/pets/MyPetsFragment.kt
+++ app/src/main/java/ru/kpfu/itis/gureva/catcare/presentation/screens/pets/MyPetsFragment.kt
@@ -1,8 +1,23 @@
 package ru.kpfu.itis.gureva.catcare.presentation.screens.pets
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import ru.kpfu.itis.gureva.catcare.R
+import ru.kpfu.itis.gureva.catcare.databinding.FragmentMyPetsBinding
 
 class MyPetsFragment : Fragment(R.layout.fragment_my_pets) {
+    private var binding: FragmentMyPetsBinding? = null
 
+    private val fragmentContainerId = R.id.main_container
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMyPetsBinding.bind(view)
+
+        parentFragmentManager.beginTransaction()
+            .replace(fragmentContainerId, PetFragment())
+            .commit()
+    }
 }
--- app/src/main/java/ru/kpfu/itis/gureva/catcare/presentation/screens/pets/PetFragment.kt
+++ app/src/main/java/ru/kpfu/itis/gureva/catcare/presentation/screens/pets/PetFragment.kt
@@ -1,8 +1,18 @@
 package ru.kpfu.itis.gureva.catcare.presentation.screens.pets
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import ru.kpfu.itis.gureva.catcare.R
+import ru.kpfu.itis.gureva.catcare.databinding.FragmentPetBinding
 
 class PetFragment : Fragment(R.layout.fragment_pet) {
+    private var binding: FragmentPetBinding? = null
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentPetBinding.bind(view)
+
+    }
 }
